7607def3f3fcae8f9bd35e3cffc361c9766e5a08
johnfn <johnfn@gmail.com>
2016-12-09 21:24:55 -0800

    Bleh

---
+++ Assets/Scripts/Editor/CustomTiledImporter.cs
@@ -0,0 +1,19 @@
+/*
+using System.Collections.Generic;
+using UnityEngine;
+
+[Tiled2Unity.CustomTiledImporter]
+class MyCustomTiledImporter : Tiled2Unity.ICustomTiledImporter {
+    public void HandleCustomProperties(GameObject gameObject,
+        IDictionary<string, string> keyValuePairs) {
+
+        Debug.Log("Handle custom properties from Tiled map");
+    }
+
+    public void CustomizePrefab(GameObject prefab) {
+        var x = 5;
+
+        Debug.Log("Customize prefab");
+    }
+}
+*/
